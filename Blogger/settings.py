b544c292218b31068b6cc3979a129061f601d845
coderrancho <ranjugithub2020@gmail.com>
2021-12-21 22:35:08 +0530

    Fifth commit

--- Blogger/settings.py
+++ Blogger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87t*piac$2e9%@glt*2dq(&avq+3epc&4=3nz8ser-ze&ravj0'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://bloggermaster.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['bloggermaster.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -122,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-import os
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,  "static"),
 # ]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
